942ada2b8c77d247efca1b9ac6f509b59f28f1b9
Sarabnoor Singh <singhsarabnoor@gmail.com>
2025-07-27 18:29:16 +0530

    Spinner added

--- cases-frontend/components/Spinner.tsx
+++ cases-frontend/components/Spinner.tsx
@@ -2,8 +2,8 @@
 
 export default function Spinner() {
   return (
-    <div className="fixed inset-0 z-50 bg-black bg-opacity-40 flex items-center justify-center">
-      <div className="w-10 h-10 border-4 border-white border-t-transparent rounded-full animate-spin"></div>
+    <div className="fixed inset-0 z-50 flex items-center justify-center pointer-events-none">
+      <div className="w-10 h-10 border-4 border-white border-t-transparent rounded-full animate-spin shadow-lg"></div>
     </div>
   );
 }
