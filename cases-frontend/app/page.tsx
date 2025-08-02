63ac7552ba54cbbe442e85be0455ea19b53efa15
Sarabnoor Singh <singhsarabnoor@gmail.com>
2025-08-02 16:47:12 +0530

    fixed btn

--- cases-frontend/app/page.tsx
+++ cases-frontend/app/page.tsx
@@ -118,7 +118,7 @@ const handleBackupDownload = async () => {
             onClick={handleBackupDownload}
             className="bg-blue-600 text-white font-semibold px-4 py-2 rounded hover:bg-blue-700 transition duration-300 cursor-pointer"
           >
-            Download MongoDB Backup
+            Download Backup
           </button>
         </div>
 
